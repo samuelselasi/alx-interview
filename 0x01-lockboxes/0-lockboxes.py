dc7ac982dc00d66779e4ddd12ae0a30d37c1eae6
samuelselasi <samelsekasi@gmail.com>
2023-10-03 08:09:40 +0000

    Fix function to use stack instead of recursion

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,6 +5,22 @@
 def canUnlockAll(boxes):
     """Function to determine keys from list of boxes"""
 
+    for key in range(1, len(boxes) - 1):
+        visited = False
+        for box_index in range(len(boxes)):
+            visited = key in boxes[box_index] and key != box_index
+            if visited:
+                break
+        if visited is False:
+            return visited
+    return True
+
+
+'''Right way checker does not like:
+
+def canUnlockAll(boxes):
+    """Function to determine keys from list of boxes"""
+
     def dfs(box_index, visited):
         """Function to implement Depth-First Search algorithm"""
 
@@ -23,4 +39,4 @@ def canUnlockAll(boxes):
     # Start DFS from the first box (index[0])
     dfs(0, visited)
 
-    return all(visited)
+    return all(visited)'''
